3037bfa7b1224c4c53d4c44510d175bb2e718960
ErikSCc <77497924+ErikSCc@users.noreply.github.com>
2021-06-01 17:57:29 +0200

    Vies and search activity
    -Defining Views
    -setup variables
    -start new activity for City search

---
+++ app/src/main/java/com/example/weatherchek/searchresult.kt
@@ -0,0 +1,11 @@
+package com.example.weatherchek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class searchresult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchresult)
+    }
+}
