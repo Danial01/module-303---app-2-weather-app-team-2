c250c8b63b01d070db1eb94a2f3d27d204b03e2e
ErikSCc <77497924+ErikSCc@users.noreply.github.com>
2021-06-05 20:58:26 +0200

    Setting up retrofit part 2

--- app/src/main/java/com/example/weatherchek/activity/MainActivity.kt
+++ app/src/main/java/com/example/weatherchek/activity/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.weatherchek.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import com.example.weatherchek.R
+import com.example.weatherchek.backend.RetrofitClient
+import com.example.weatherchek.model.Weather
+import com.example.weatherchek.model.weathercity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +29,28 @@ class MainActivity : AppCompatActivity() {
         searchButton?.setOnClickListener {
             val intent = Intent(this, searchresult::class.java)
             startActivity(intent)
+            getTheWeatherForCity("gothenburg")
         }
 
+    }
+    private fun getTheWeatherForCity(city:String){
+        RetrofitClient
+            .instance
+            .getweather(city)
+            .enqueue( object : Callback<List<Weather>>{
+                override fun onResponse(
+                    call: Call<List<Weather>>,
+                    response: Response<List<Weather>>
+                ) {
+                    val listOfWeather=response.body() as ArrayList<Weather>
+                }
+
+                override fun onFailure(call: Call<List<Weather>>, t: Throwable) {
+                    Log.e(TAG,"Not getting Data")
+                }
+            })
+    }
+    companion object {
+        private val TAG=MainActivity::class.java.simpleName
     }
 }
--- app/src/main/java/com/example/weatherchek/backend/EndPoint.kt
+++ app/src/main/java/com/example/weatherchek/backend/EndPoint.kt
@@ -1,9 +1,11 @@
 package com.example.weatherchek.backend
 
+import com.example.weatherchek.model.Weather
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface EndPoint {
     @GET("forecast?id=524901&appid={cityname}")
-    fun getweather(@Path("cityname") nameofcity:String)
+    fun getweather(@Path("cityname") nameofcity:String): Call<List<Weather>>
 }
