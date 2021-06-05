ead5301e8a81c612a5a6ae49330993cc12a779bf
ErikSCc <77497924+ErikSCc@users.noreply.github.com>
2021-06-05 21:01:43 +0200

    Update EndPoint.kt

--- app/src/main/java/com/example/weatherchek/backend/EndPoint.kt
+++ app/src/main/java/com/example/weatherchek/backend/EndPoint.kt
@@ -1,16 +1,11 @@
 package com.example.weatherchek.backend
 
-import com.example.weatherchek.model.Weather
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface EndPoint {
-<<<<<<< Updated upstream
+
     @GET("forecast?id=524901&appid={cityname}")
-    fun getweather(@Path("cityname") nameofcity:String): Call<List<Weather>>
-=======
-    @GET("forecast?id=524901&appid={cityName}")
-    fun getCityWeather(@Path("cityName") nameOfCity: String)
->>>>>>> Stashed changes
+    fun getweather(@Path("cityname") nameofcity: String): Call<List<Weather>>
 }
