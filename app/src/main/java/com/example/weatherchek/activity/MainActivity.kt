a7106badae1a23d7dcda70a376de70dd81cfe003
ErikSCc <77497924+ErikSCc@users.noreply.github.com>
2021-06-13 19:58:33 +0200

    adding spinner

--- app/src/main/java/com/example/weatherchek/activity/MainActivity.kt
+++ app/src/main/java/com/example/weatherchek/activity/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.weatherchek.activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.view.View
+import android.widget.*
 import com.example.weatherchek.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,20 +18,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        nameOfCityEdittext = findViewById(R.id.editTextCity)
-        searchButton = findViewById(R.id.buttonSearchCity)
-       
-        searchButton?.setOnClickListener {
-            val cityName = nameOfCityEdittext?.text.toString().toLowerCase().trim()
-            if (cityName == "oslo" || cityName == "gothenburg" ||
-                cityName == "london" || cityName == "panama"
+        //nameOfCityEdittext = findViewById(R.id.editTextCity)
+        //searchButton = findViewById(R.id.buttonSearchCity)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.weather_city,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                 spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener= object:AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
             ) {
-                val intent = Intent(this@MainActivity, SearchResult::class.java)
-                intent.putExtra("cityName", editTextCity.text.toString())
-                startActivity(intent)
-            } else {
-                editTextCity?.setError("Something went wrong! Enter new city!")
+                val cityName =  parent?.getItemAtPosition(position).toString().trim()
+                if (cityName == "Oslo" || cityName == "Gothenburg" ||
+                    cityName == "London" || cityName == "Panama"
+                ) {
+                    val intent = Intent(this@MainActivity, SearchResult::class.java)
+                    intent.putExtra("cityName",cityName)
+                    startActivity(intent)
+
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
             }
         }
-    }
-}
+
+        }
+
+
+       /* searchButton?.setOnClickListener {
+
+
+        }
+        }*/
+ }
+
+
